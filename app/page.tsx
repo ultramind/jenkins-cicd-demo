777a6922c5312056d0317575c178cfddf72a1689
ultramind <udechukwuakachukwu@gmail.com>
2025-05-29 12:16:47 +0100

    Initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,24 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
+        <div className="flex gap-5">
+          <Image
+            className="dark:invert"
+            src="/next.svg"
+            alt="Next.js logo"
+            width={180}
+            height={38}
+            priority
+          />
+          <Image
+            className="rounded-2xl"
+            src="/jenkins-logo.png"
+            alt="Jenkins logo"
+            width={100}
+            height={38}
+            priority
+          />
+        </div>
         <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2 tracking-[-.01em]">
             Get started by editing{" "}
